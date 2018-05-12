a856cb263ead5d197c204702756832dcab76b54a
Gaston Andrade <gandrade.mc@gmail.com>
2018-05-11 23:16:46 -0300

    inicio

---
+++ RecuperosYMandatos/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+using System.Threading;
+using System.Globalization;
+
+namespace RecuperosYMandatos
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public static string Language
+        {
+            get
+            {
+                var currentContext = new HttpContextWrapper(HttpContext.Current);
+                var routeData = RouteTable.Routes.GetRouteData(currentContext);
+                if (routeData != null)
+                {
+                    var lang = (string)routeData.Values["culture"];
+                    if (string.IsNullOrEmpty(lang))
+                    {
+                        lang = "es-MX";
+                    }
+                    return lang;
+                }
+                return "es-MX";
+            }
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(MvcApplication.Language);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(MvcApplication.Language);
+        }
+    }
+}
---
+++ RecuperosYMandatos/Scripts/Aplicacion/Deudor.js
@@ -0,0 +1,18 @@
+﻿var deudor = {
+     confirmarDatoIdentificacion: function (url, contenedor, contenedorModal) {
+        var errores = [];
+        var model = build.renderizarModelFrom(contenedor);
+        errores = deudor.validarCampoIdentificacion(model, errores);
+        if (errores.length != 0) {
+            custom.setearLoader();
+            modals.gernericoListErrores(contenedorModal, "Advertencia", errores);
+        } else {
+            url = url + "?ID=" + model.ID;
+            $(location).attr('href', url);
+        }
+     },
+     validarCampoIdentificacion: function (model, lisErroes) {
+        if (!model.ID) { lisErroes.push("Por favor, ingrese su identificación."); }
+        return lisErroes;
+     }
+}
